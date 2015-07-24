46fd6152f35712d8fc4a77d9e9acc70d633bfb33
George H. Alvares <blackbird.fly95@gmail.com>
2015-07-23 23:00:45 -0300

    just cleaning up

--- app.js
+++ app.js
@@ -8,16 +8,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', function(socket) {
-	//console.log('Component connected.');
-
-	socket.on('disconnect', function() {
-		//console.log('Component disconnected.');
-	});
-
 	socket.on('pressKey', function(data) {
 		socket.broadcast.emit('keyPressed', data);
 	});
-
 });
 
 http.listen(3000, function() {
